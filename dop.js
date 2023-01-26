c6425608fe44fd6b88b973c39bfe543c9fe18e14
h1ut <amirosmonalievgg@gmail.com>
2023-01-26 18:58:05 +0600

    dz

---
+++ dop.js
@@ -0,0 +1,19 @@
+//1
+qq =['alex', 'anna', 'oleg']
+qq.push(1,2,3)
+console.log(qq);
+
+
+//2
+
+qq =['alex', 'anna', 'oleg']
+console.log(qq[0]);
+
+
+//3
+
+let arr1 = [1, true, false, undefined]
+arr1.shift(0)
+arr1.shift(1)
+arr1.unshift("Sasha" , "Alina")
+console.log(arr1);
